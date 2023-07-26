83b85d3e936e985f705017ff34f0ecdd43414737
Selikeyt <116385167+Selikeyt@users.noreply.github.com>
2023-07-26 22:28:05 +0500

    Add files via upload

---
+++ NewsPortal/news/views.py
@@ -0,0 +1,16 @@
+from django.views.generic import ListView, DetailView
+
+from .models import Post
+
+
+class PostsList(ListView):
+    model = Post
+    ordering = 'title'
+    template_name = 'news.html'
+    context_object_name = 'news'
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'new.html'
+    context_object_name = 'new'
